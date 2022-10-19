929348de70477366300583a42620a24efc6391bf
Samuel JS Wright <swrig109@uncc.edu>
2022-10-19 00:27:42 -0400

    bed

--- sankey.py
+++ sankey.py
@@ -14,5 +14,22 @@ def combinegrades(df, combine = ["AB", "DF"], group = ["A", "F"]):
     for c, g in zip(combine, group):
         df[df.isin(list(c))] = g
 #%%
-
+def countsdf(df):
+    temp = df.value_counts(dropna = False)
+    out = pd.DataFrame(temp)
+    out.reset_index(inplace=True)
+    out.rename(columns = {out.columns[-1]: "Count"}, inplace = True)
+    return out
+#%%
+def rename(df):
+    for col in df.columns:
+        if df[col].dtype == "O":
+            df[col] = df[col] + "_" + col
+#%%
+def get_labels(df):
+    lab = []
+    for col in df:
+        temp = df[col].unique()
+        lab.append(temp)
+    return lab
 #%%
