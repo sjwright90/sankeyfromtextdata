2309cda9e91af4da6274ae28e1344fb46283d45e
Samuel JS Wright <swrig109@uncc.edu>
2022-10-28 22:27:09 -0400

    rewrite docstrings

--- sankey.py
+++ sankey.py
@@ -21,104 +21,183 @@ import plotly.graph_objects as go
 import itertools
 from seaborn import color_palette as sns_pal
 #%%
-def make_clr_tup(palete = "Set1", alpha = 0.5):
-    '''Takes a color set from seaborn (default is "Set3" from colorbrewer) and 
-    transforms it into a list of strings in the form: "rgba(1,1,1,1)" 
+def make_clr_tup(palette = "Set1", alpha = 0.5):
+    '''Transforms Seaborn color palette into appropriate format for plotly plotting
     
-    Args:
-        palete: string object of a named color palete available in seaborn
-        alpha: float 0.0-1.0, the alpha level to set for all colors
+    Parameters:
+    ----------
+    palette : string
+        A seaborn color palette
+            
+    alpha: float {0.0-1.0}
+        The alpha level to set for all colors
+    
+    
     Returns:
-        List of strings in the form "rgba(x,x,x,alpha)
+    ----------
+    clrpl :
+        List of strings in the form "rgba(x,x,x,alpha)"
     '''
-    clrpl = [list(c) + [alpha] for c in sns_pal(palete)]
+    
+    
+    clrpl = [list(c) + [alpha] for c in sns_pal(palette)]
     clrpl = ["rgba" + str(tuple(c)) for c in clrpl]
     return clrpl
 
+
 def combinegrades(df, combine = ["AB", "DF"], group = ["A", "F"]):
-    '''Groups fields in data frame based on input. 
+    '''Rename categorical features in data frame by grouping. 
     
-    Args:
-        combine: list of strings to be combined. Strings to be grouped written all as one
-            string (ie "AB" will be "A" and "B").
-        group: list of strings to replace strings identified by the combine, needs to be same length
-            as combine
+    Parameters:
+    ----------
+    
+    combine : list of strings
+        Strings to be grouped written all as one string (eg "AB" will be "A" and "B").
+        
+    group : list of strings
+        strings to replace with, needs to be same length as combine
     '''
+    
+    
     for c, g in zip(combine, group):
         df[df.isin(list(c))] = g
 
+
 def countsdf(df):
-    '''Applies value_counts to entire data frame, then converts results into a new data frame, 
+    '''Get each unique path through the data frame and number of each pathway
+    
+    Applies value_counts to entire data frame, then converts results into a new data frame, 
     and renames final column to "Count". Gives count of each unique path through data frame. 
     
-    Args:
-        df: a pandas data frame
-    Returns:
-        pandas data frame'''
+    Parameters:
+    ----------
+    
+    df : pandas dataframe
+    
+    
+    Returns :
+    ----------
+    
+    out : pandas dataframe'''
+    
+    
     temp = df.value_counts(dropna = False)
     out = pd.DataFrame(temp)
     out.reset_index(inplace=True)
     out.rename(columns = {out.columns[-1]: "Count"}, inplace = True)
     return out
 
+
 def rename(df):
-    '''Renames each item in data frame by adding the column name to all the strings in that column. 
+    '''Renames each item in data frame using column names. 
     
-    Args:
-        df: a pandas dataframe
+    Parameters:
+    ----------
+    
+    df: pandas dataframe
+    
     '''
+    
+    
     for col in df.columns:
         if df[col].dtype == "O":
             df[col] = df[col] + "_" + col
 
+            
 def get_labels(df):
     '''Gets all unique string values in the data frame. 
     
-    Args:
-        df: a pandas dataframe
+    Parameters:
+    ----------
+    
+    df : pandas dataframe
+    
+    
     Returns:
-        list: list of strings of all unique string values in the data frame'''
+    ----------
+    
+    list : list of strings
+    
+    '''
+    
+    
     lab = [df[col].unique() for col in df if col != "Count"]
     lab = list(itertools.chain.from_iterable(lab))
     lab = [v for v in lab if str(v).lower() != "nan"]
     return lab
 
+
 def make_indexing(df, labels):
     '''Replaces all unique labels in a data frame with an integer value corresponding to that 
     label's index location in a list of the unique labels. 
     
-    Args:
-        df: pandas data frame
-        labels: list of all unique strings in the data frame
+    Parameters:
+    ----------
+    
+    df : pandas data frame
+    
+    labels : list of strings
+    
+        list of all unique strings in the dataframe
+    
+    
     Returns:
-        df_c: copy of the data frame with the strings recoded as numerical (presence of nan will cause int to 
-            automatically be converted to floats)
-        to_repl: a dictionary mapping strings to replacement values'''
+    ----------
+    
+    df_c : pandas data frame
+    
+        copy of the data frame with the strings replaced with numerical or np.NaN
+            
+    to_repl : dict
+        a dictionary mapping strings to replacement values
+    
+    '''
+    
+    
     df_c = df.copy()
     index_val = np.arange(len(labels))
     to_repl = dict(zip(labels, index_val))
     df_c.replace(to_repl, inplace = True)
     return df_c, to_repl
 
+
 def get_source_target_value(df, color = False, colorset = None, alpha = 0.5):
     '''Creates three lists of integers for input into Sankey plotter 
     
-    Args:
-        df: a pandas data frame consisting only of int and np.nan
-        color: bool, default False. Use True if you want to get 
+    Parameters:
+    ----------
+    
+    df : a pandas data frame consisting only of int and np.nan
+    
+    color : bool, default False. Use True if you want to get 
             a color list for the links, by default will give 
             one color for each unique first node
-        colorset: default None, by default will use colorbrewer Set1 palete, 
+            
+    colorset : default None, by default will use colorbrewer Set1 palete, 
             replace with appropriate string of palete from Seaborn library
             to change from default palete
-        alpha: float default 0.5, alpha value for the links colors
+    alpha : float default 0.5, alpha value for the links colors
+    
+    
     Returns:
-        source: list of int corresponding to points in a Sankey diagram that
+    ----------
+    
+    source: list
+        list of int corresponding to nodes in a Sankey diagram that
             have links coming out of them
-        target: list of int corresponding to points in a Sankey diagram that
-            have links coming into them
-        value: thickness of a links connecting source and target
-        '''
+            
+    target : list
+    
+        list of integers corresponding to nodes in a Sankey diagram that
+        have links feeding into them
+        
+    value : list
+    
+        thickness of a links connecting source and target
+        
+    '''
+    
+    
     if color:
         if isinstance(colorset, str):
             if alpha >= 0 and alpha <= 1:
@@ -146,17 +225,32 @@ def node_colors(labels, plotcolors, alpha = None):
     '''Creates a list to color the nodes by, based on labels and current
     colors allocated to the links, all other nodes will be grey. 
     
-    Args:
-        labels: list of labels that will be used in the Sankey diagram
-        plotcolors: colors already generated to color the links in the diagram
-        alpha: default None, change to float between 0-1 to change default
-            0.5 for alpha value of node color'''
+    Parameters:
+    ----------
+        
+    labels : list-like
+    
+        list of labels that will be used in the Sankey diagram
+        
+    plotcolors : list-like
+    
+        colors already generated to color the links in the diagram
+        
+    alpha : default None
+    
+        change to float between 0-1 to define alpha of node color, default alpha is 0.5
+        
+    '''
+    
     
     node_color = list(dict.fromkeys(plotcolors))
+    
     if alpha and alpha<=1 and alpha >=0:
         alpha = str(alpha) + ")"
         node_color = [color.replace("0.5)", alpha) for color in node_color]
+        
     base = "rgba(0.5,0.5,0.5,0.3)"
+    
     while len(node_color) < len(labels):
         node_color.append(base)
    
@@ -166,20 +260,40 @@ def node_colors(labels, plotcolors, alpha = None):
 def plot_sankeyd(labels,src, trg, val, save = False, plotcolors = None, nodecolors = None, show_fig = False):
     '''Plots a Sankey diagram. 
     
-    Args:
-        labels: list of strings, labels for each node in Sankey
-        src: source nodes
-        trg: target nodes
-        val: thickness of link connecting source nodes and target nodes
-        save: bool, default False, use True if you want to export the file to html.
-            If True will prompt for file path
-        plotcolors: default None, if given list of rgba color strings 
-            will color links using the list
-        nodecolors: default None, if given list of rgba color strings
-            will color nodes accordingly, see node_colors() function for
-            making the list
-        show_fig: bool, default False, display the figure
-        '''
+    Parameters:
+    ----------
+    
+    labels : list-like
+        List of strings, labels for each node in Sankey
+    
+    src : list-like
+        List of integers, source nodes
+    
+    trg : list-like
+        List of integers, target nodes
+    
+    val : list-like
+        List of integers, thickness of link connecting source nodes and target nodes
+        
+    save : bool 
+        Default False, use True if to export the plot to html. If True will prompt for file path.
+    
+    plotcolors : list-like
+        Default None, if given list of colors will color links using the list
+        
+    nodecolors : list-like
+        Default None, if given list of colors will color nodes accordingly
+            
+    show_fig : bool
+        Default False, use True to display the figure
+    
+    Returns:
+    ----------
+    
+    fig : plotly object
+        Returned if "save" set to True
+        
+    '''
     labels = [item.replace("_", " ") for item in labels]
     nodesdict = dict(label = labels)
     if nodecolors: nodesdict["color"] = nodecolors
