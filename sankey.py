dab9d3c8d4649f10ddd4887e86d5d62e537bf927
Samuel JS Wright <swrig109@uncc.edu>
2022-10-29 17:02:52 -0400

    fixing code formatting

--- sankey.py
+++ sankey.py
@@ -3,18 +3,7 @@ Created on Sat Jul 23 10:17:58 2022
 @author: Samsonite
 """
 #%%
-'''This file contains a series of functions used to transform and manipulate a csv or xlsx file into
-a format appropriate for drawing a Sankey diagram and then producing said sankey diagram.
-The tabular data does already need to be in a specific format. Each column should represent 
-one event (in this case a given class offered in a given year). Each row will be an individual 
-path through said events (in this case the grades a student got in a given class). Each cell 
-contains the given result of the individual and that event (for example a "B" in "Calc1_2020"). 
-The functions were built assuming string data would be the input, but I think numerical data 
-would work. The intent of this work was to make a Sankey plot to track how students flowed 
-through a series of college courses, the data set provided as an example is just randomly 
-generated data, but shows how the process works. I believe the code should be highly adaptable 
-to any instance where one is trying to make a sankey plot of categorical data, again some 
-formating of your data set might be needed before it will work though.'''
+
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
